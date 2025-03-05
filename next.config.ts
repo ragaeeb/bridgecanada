24c4a3aacef34caed165767c1025e061d5936273
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2025-03-05 16:51:29 -0500

    Fix images not showing up and bump version

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+    basePath: '/bridgecanada', // we need this since GitHub pages will put all assets in a folder: ragaeeb.github.io/bridgecanada/*
+    images: {
+        unoptimized: true, // Disable server-based image optimization. Next.js does not support dynamic features with static exports
+    },
     output: 'export', // enables static exports which is needed for GitHub Pages
 };
 
