53ea9386da04fc630a75feb0c9fb3d117c7311ea
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2025-03-05 17:45:51 -0500

    Clear out nextjs config and add it to workflow

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-    basePath: process.env.NODE_ENV === 'production' ? '/bridgecanada' : '',
-    images: {
-        unoptimized: true, // Disable server-based image optimization. Next.js does not support dynamic features with static exports
-    },
-    output: 'export', // enables static exports which is needed for GitHub Pages
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
