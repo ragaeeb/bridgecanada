85d8ec6b959d020932f9487c0a3ae8828c2a3d67
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2025-03-05 20:32:18 -0500

    Removing configs for github pages since we will deploy to netlify

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    images: {
+        unoptimized: true, // Disable server-based image optimization. Next.js does not support dynamic features with static exports
+    },
+    output: 'export', // enables static exports which is needed for GitHub Pages
+    trailingSlash: true,
+};
 
 export default nextConfig;
