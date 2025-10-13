5139c47cd0476bd301e70d3057d8ff51c4220b96
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2025-10-13 18:43:51 -0400

    Migrate to vercel

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,3 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-    plugins: {
-        '@tailwindcss/postcss': {},
-    },
-};
+const config = { plugins: ['@tailwindcss/postcss'] };
 
 export default config;
